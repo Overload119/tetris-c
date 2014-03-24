01aa5cb4c30b5e0f2c39262bccdc7f56ab43863f
Ben Carroll <benjamincarroll93@gmail.com>
2014-03-24 19:40:24 -0400

    Grid.h updated

--- block.h
+++ block.h
@@ -1,4 +1,15 @@
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
 #include "cell.h"
+#include <stdbool.h>
 
+class block{
+  cell *cells[4];
+public:
+  void clockwise();
+  void counterClockwise();
+  void moveLeft();
+  void moveRight();
+  bool isStopped();
+  void drop();
+}
