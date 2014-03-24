fa967d63c136db99f8ac69284086a7533fa13725
Ben Carroll <benjamincarroll93@gmail.com>
2014-03-24 19:21:20 -0400

    type S class filled out

--- block.cc
+++ block.cc
@@ -14,5 +14,15 @@ Block::Block(char type){
     cells[1] = getCellAt(1,4);
     cells[2] = getCellAt(2,4);
     cells[3] = getCellAt(2,3);
+  } else if (type == 'O'){
+    cells[0] = getCellAt(0,3);
+    cells[1] = getCellAt(1,3);
+    cells[2] = getCellAt(0,4);
+    cells[3] = getCellAt(1,4);
+  } else if (type == 'S'){
+    cells[0] = getCellAt(0,4);
+    cells[1] = getCellAt(1,4);
+    cells[2] = getCellAt(1,3);
+    cells[3] = getCellAt(2,3);
   }
 }
