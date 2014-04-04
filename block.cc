6fd8bccac2a2474ad4834849c73ebaaa5252e26a
Overload119 <budgeneration@gmail.com>
2014-04-04 12:16:57 -0400

    clean up code

--- block.cc
+++ block.cc
@@ -67,8 +67,8 @@ bool Block::isValid() {
   return validState;
 }
 
-bool Block::isCellValid(int x, int y){
-  //checks if cell below is already true
+bool Block::isCellValid(int x, int y) {
+  // Check if the cell is active (ie turned on) and does not belong to this Block
   Cell *tempCell = grid->getCellAt(x, y);
   if (tempCell == NULL) {
     return true;
@@ -206,216 +206,215 @@ int Block::getColor() {
   return color;
 }
 
-bool Block::counterClockwise() {
+void Block::counterClockwise() {
   int newX[4];
   int newY[4];
   char c = cells[1]->getChar();
-    if (c == 'O'){
-      return false;
-    } else if (c == 'I'){
-      if ((rotationPosition % 2) == 0){
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY();
-        newX[1] = cells[1]->getX() -1;
-        newY[1] = cells[1]->getY() -1;
-        newX[2] = cells[2]->getX() -2;
-        newY[2] = cells[2]->getY() -2;
-        newX[3] = cells[3]->getX() -3;
-        newY[3] = cells[3]->getY() -3;
-      } else {
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY();
-        newX[1] = cells[1]->getX() +1;
-        newY[1] = cells[1]->getY() +1;
-        newX[2] = cells[2]->getX() +2;
-        newY[2] = cells[2]->getY() +2;
-        newX[3] = cells[3]->getX() +3;
-        newY[3] = cells[3]->getY() +3;
-      }
-    } else if (c == 'Z'){
-      if ((rotationPosition % 2) == 0){
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY()+1;
-        newX[1] = cells[1]->getX()-1;
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY()-1;
-        newX[3] = cells[3]->getX()-1;
-        newY[3] = cells[3]->getY()-2;
-      } else {
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY()-1;
-        newX[1] = cells[1]->getX()+1;
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY()+1;
-        newX[3] = cells[3]->getX()+1;
-        newY[3] = cells[3]->getY()+2;
-      }
-    } else if (c == 'L'){
-      if (rotationPosition == 0){
-        newX[0] = cells[0]->getX()+1;
-        newY[0] = cells[0]->getY();
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY()-1;
-        newX[2] = cells[2]->getX()-1;
-        newY[2] = cells[2]->getY()-2;
-        newX[3] = cells[3]->getX()-2;
-        newY[3] = cells[3]->getY()-1;
-      } else if (rotationPosition == 1){
-        newX[0] = cells[0]->getX()+1;
-        newY[0] = cells[0]->getY()-1;
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX()-1;
-        newY[2] = cells[2]->getY()+1;
-        newX[3] = cells[3]->getX();
-        newY[3] = cells[3]->getY()+2;
-      } else if (rotationPosition == 2){
-        newX[0] = cells[0]->getX()-2;
-        newY[0] = cells[0]->getY()-1;
-        newX[1] = cells[1]->getX()-1;
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY()+1;
-        newX[3] = cells[3]->getX()+1;
-        newY[3] = cells[3]->getY();
-      } else if (rotationPosition == 3){
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY()+2;
-        newX[1] = cells[1]->getX()+1;
-        newY[1] = cells[1]->getY()+1;
-        newX[2] = cells[2]->getX()+2;
-        newY[2] = cells[2]->getY();
-        newX[3] = cells[3]->getX()+1;
-        newY[3] = cells[3]->getY()-1;
-      }
-    } else if (c == 'T'){
-      if (rotationPosition == 0){
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY()+1;
-        newX[1] = cells[1]->getX()-1;
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX()-2;
-        newY[2] = cells[2]->getY()-1;
-        newX[3] = cells[3]->getX();
-        newY[3] = cells[3]->getY()-1;
-      } else if (rotationPosition == 1) {
-        newX[0] = cells[0]->getX()+2;
-        newY[0] = cells[0]->getY();
-        newX[1] = cells[1]->getX()+1;
-        newY[1] = cells[1]->getY()+1;
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY()+2;
-        newX[3] = cells[3]->getX();
-        newY[3] = cells[3]->getY();
-      } else if (rotationPosition == 2) {
-        newX[0] = cells[0]->getX()-1;
-        newY[0] = cells[0]->getY()-2;
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY()-1;
-        newX[2] = cells[2]->getX()+1;
-        newY[2] = cells[2]->getY();
-        newX[3] = cells[3]->getX()-1;
-        newY[3] = cells[3]->getY();
-      } else if (rotationPosition == 3) {
-        newX[0] = cells[0]->getX()-1;
-        newY[0] = cells[0]->getY()+1;
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX()+1;
-        newY[2] = cells[2]->getY()-1;
-        newX[3] = cells[3]->getX()+1;
-        newY[3] = cells[3]->getY()+1;
-      }
-    } else if (c == 'J'){
-      if (rotationPosition == 0){
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY()+1;
-        newX[1] = cells[1]->getX()+1;
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY()-1;
-        newX[3] = cells[3]->getX()-1;
-        newY[3] = cells[3]->getY()-2;
-      } else if (rotationPosition == 1) {
-        newX[0] = cells[0]->getX()+2;
-        newY[0] = cells[0]->getY();
-        newX[1] = cells[1]->getX()+1;
-        newY[1] = cells[1]->getY()-1;
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY();
-        newX[3] = cells[3]->getX()-1;
-        newY[3] = cells[3]->getY()+1;
-      } else if (rotationPosition == 2) {
-        newX[0] = cells[0]->getX()-1;
-        newY[0] = cells[0]->getY()-2;
-        newX[1] = cells[1]->getX()-2;
-        newY[1] = cells[1]->getY()-1;
-        newX[2] = cells[2]->getX()-1;
-        newY[2] = cells[2]->getY();
-        newX[3] = cells[3]->getX();
-        newY[3] = cells[3]->getY()+1;
-      } else if (rotationPosition == 3) {
-        newX[0] = cells[0]->getX()-1;
-        newY[0] = cells[0]->getY()+1;
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY()+2;
-        newX[2] = cells[2]->getX()+1;
-        newY[2] = cells[2]->getY()+1;
-        newX[3] = cells[3]->getX()+2;
-        newY[3] = cells[3]->getY();
-      }
-    } else if (c == 'S'){
-      if (rotationPosition == 0){
-        newX[0] = cells[0]->getX()+1;
-        newY[0] = cells[0]->getY();
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY()-1;
-        newX[2] = cells[2]->getX()-1;
-        newY[2] = cells[2]->getY();
-        newX[3] = cells[3]->getX()-2;
-        newY[3] = cells[3]->getY()-1;
-      } else if (rotationPosition == 1) {
-        newX[0] = cells[0]->getX()+1;
-        newY[0] = cells[0]->getY()-1;
-        newX[1] = cells[1]->getX();
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX()+1;
-        newY[2] = cells[2]->getY()+1;
-        newX[3] = cells[3]->getX();
-        newY[3] = cells[3]->getY()+2;
-      } else if (rotationPosition == 2) {
-        newX[0] = cells[0]->getX()-2;
-        newY[0] = cells[0]->getY()-1;
-        newX[1] = cells[1]->getX()-1;
-        newY[1] = cells[1]->getY();
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY()-1;
-        newX[3] = cells[3]->getX()+1;
-        newY[3] = cells[3]->getY();
-      } else if (rotationPosition == 3) {
-        newX[0] = cells[0]->getX();
-        newY[0] = cells[0]->getY()+2;
-        newX[1] = cells[1]->getX()+1;
-        newY[1] = cells[1]->getY()+1;
-        newX[2] = cells[2]->getX();
-        newY[2] = cells[2]->getY();
-        newX[3] = cells[3]->getX()+1;
-        newY[3] = cells[3]->getY()-1;
-      }
+  if (c == 'O') {
+    return false;
+  } else if (c == 'I'){
+    if ((rotationPosition % 2) == 0){
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY();
+      newX[1] = cells[1]->getX() -1;
+      newY[1] = cells[1]->getY() -1;
+      newX[2] = cells[2]->getX() -2;
+      newY[2] = cells[2]->getY() -2;
+      newX[3] = cells[3]->getX() -3;
+      newY[3] = cells[3]->getY() -3;
+    } else {
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY();
+      newX[1] = cells[1]->getX() +1;
+      newY[1] = cells[1]->getY() +1;
+      newX[2] = cells[2]->getX() +2;
+      newY[2] = cells[2]->getY() +2;
+      newX[3] = cells[3]->getX() +3;
+      newY[3] = cells[3]->getY() +3;
     }
+  } else if (c == 'Z'){
+    if ((rotationPosition % 2) == 0){
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY()+1;
+      newX[1] = cells[1]->getX()-1;
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY()-1;
+      newX[3] = cells[3]->getX()-1;
+      newY[3] = cells[3]->getY()-2;
+    } else {
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY()-1;
+      newX[1] = cells[1]->getX()+1;
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY()+1;
+      newX[3] = cells[3]->getX()+1;
+      newY[3] = cells[3]->getY()+2;
+    }
+  } else if (c == 'L'){
+    if (rotationPosition == 0){
+      newX[0] = cells[0]->getX()+1;
+      newY[0] = cells[0]->getY();
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY()-1;
+      newX[2] = cells[2]->getX()-1;
+      newY[2] = cells[2]->getY()-2;
+      newX[3] = cells[3]->getX()-2;
+      newY[3] = cells[3]->getY()-1;
+    } else if (rotationPosition == 1){
+      newX[0] = cells[0]->getX()+1;
+      newY[0] = cells[0]->getY()-1;
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX()-1;
+      newY[2] = cells[2]->getY()+1;
+      newX[3] = cells[3]->getX();
+      newY[3] = cells[3]->getY()+2;
+    } else if (rotationPosition == 2){
+      newX[0] = cells[0]->getX()-2;
+      newY[0] = cells[0]->getY()-1;
+      newX[1] = cells[1]->getX()-1;
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY()+1;
+      newX[3] = cells[3]->getX()+1;
+      newY[3] = cells[3]->getY();
+    } else if (rotationPosition == 3){
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY()+2;
+      newX[1] = cells[1]->getX()+1;
+      newY[1] = cells[1]->getY()+1;
+      newX[2] = cells[2]->getX()+2;
+      newY[2] = cells[2]->getY();
+      newX[3] = cells[3]->getX()+1;
+      newY[3] = cells[3]->getY()-1;
+    }
+  } else if (c == 'T'){
+    if (rotationPosition == 0){
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY()+1;
+      newX[1] = cells[1]->getX()-1;
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX()-2;
+      newY[2] = cells[2]->getY()-1;
+      newX[3] = cells[3]->getX();
+      newY[3] = cells[3]->getY()-1;
+    } else if (rotationPosition == 1) {
+      newX[0] = cells[0]->getX()+2;
+      newY[0] = cells[0]->getY();
+      newX[1] = cells[1]->getX()+1;
+      newY[1] = cells[1]->getY()+1;
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY()+2;
+      newX[3] = cells[3]->getX();
+      newY[3] = cells[3]->getY();
+    } else if (rotationPosition == 2) {
+      newX[0] = cells[0]->getX()-1;
+      newY[0] = cells[0]->getY()-2;
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY()-1;
+      newX[2] = cells[2]->getX()+1;
+      newY[2] = cells[2]->getY();
+      newX[3] = cells[3]->getX()-1;
+      newY[3] = cells[3]->getY();
+    } else if (rotationPosition == 3) {
+      newX[0] = cells[0]->getX()-1;
+      newY[0] = cells[0]->getY()+1;
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX()+1;
+      newY[2] = cells[2]->getY()-1;
+      newX[3] = cells[3]->getX()+1;
+      newY[3] = cells[3]->getY()+1;
+    }
+  } else if (c == 'J'){
+    if (rotationPosition == 0){
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY()+1;
+      newX[1] = cells[1]->getX()+1;
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY()-1;
+      newX[3] = cells[3]->getX()-1;
+      newY[3] = cells[3]->getY()-2;
+    } else if (rotationPosition == 1) {
+      newX[0] = cells[0]->getX()+2;
+      newY[0] = cells[0]->getY();
+      newX[1] = cells[1]->getX()+1;
+      newY[1] = cells[1]->getY()-1;
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY();
+      newX[3] = cells[3]->getX()-1;
+      newY[3] = cells[3]->getY()+1;
+    } else if (rotationPosition == 2) {
+      newX[0] = cells[0]->getX()-1;
+      newY[0] = cells[0]->getY()-2;
+      newX[1] = cells[1]->getX()-2;
+      newY[1] = cells[1]->getY()-1;
+      newX[2] = cells[2]->getX()-1;
+      newY[2] = cells[2]->getY();
+      newX[3] = cells[3]->getX();
+      newY[3] = cells[3]->getY()+1;
+    } else if (rotationPosition == 3) {
+      newX[0] = cells[0]->getX()-1;
+      newY[0] = cells[0]->getY()+1;
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY()+2;
+      newX[2] = cells[2]->getX()+1;
+      newY[2] = cells[2]->getY()+1;
+      newX[3] = cells[3]->getX()+2;
+      newY[3] = cells[3]->getY();
+    }
+  } else if (c == 'S'){
+    if (rotationPosition == 0){
+      newX[0] = cells[0]->getX()+1;
+      newY[0] = cells[0]->getY();
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY()-1;
+      newX[2] = cells[2]->getX()-1;
+      newY[2] = cells[2]->getY();
+      newX[3] = cells[3]->getX()-2;
+      newY[3] = cells[3]->getY()-1;
+    } else if (rotationPosition == 1) {
+      newX[0] = cells[0]->getX()+1;
+      newY[0] = cells[0]->getY()-1;
+      newX[1] = cells[1]->getX();
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX()+1;
+      newY[2] = cells[2]->getY()+1;
+      newX[3] = cells[3]->getX();
+      newY[3] = cells[3]->getY()+2;
+    } else if (rotationPosition == 2) {
+      newX[0] = cells[0]->getX()-2;
+      newY[0] = cells[0]->getY()-1;
+      newX[1] = cells[1]->getX()-1;
+      newY[1] = cells[1]->getY();
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY()-1;
+      newX[3] = cells[3]->getX()+1;
+      newY[3] = cells[3]->getY();
+    } else if (rotationPosition == 3) {
+      newX[0] = cells[0]->getX();
+      newY[0] = cells[0]->getY()+2;
+      newX[1] = cells[1]->getX()+1;
+      newY[1] = cells[1]->getY()+1;
+      newX[2] = cells[2]->getX();
+      newY[2] = cells[2]->getY();
+      newX[3] = cells[3]->getX()+1;
+      newY[3] = cells[3]->getY()-1;
+    }
+  }
 
-        rotationPosition += 1;
-        rotationPosition = rotationPosition % 4;
-bool answer;
+  rotationPosition += 1;
+  rotationPosition = rotationPosition % 4;
 
   for (int i = 0; i < 4; i++){
-      answer = isCellValid(newX[i], newY[i]);
-      if (answer){
-        rotationPosition -= 1;
-        return answer;
-      }
+    cell = isCellValid(newX[i], newY[i]);
+    if (cell) {
+      rotationPosition -= 1;
+      return;
+    }
   }
 
   // Turn of the cells for the block since
--- block.h
+++ block.h
@@ -10,7 +10,7 @@ class Block{
   bool validState;
 public:
   void clockwise();
-  bool counterClockwise();
+  void counterClockwise();
   bool moveLeft();
   bool moveRight();
   bool moveDown();
